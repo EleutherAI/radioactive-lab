a74a260889ace7d74be17e321878b328482048a9
researcher2 <32028883+researcher2@users.noreply.github.com>
2020-09-09 03:43:24 +1000

    Full Refactor
    Too much to describe, but rebuilt the repo to contain a refined example on CIFAR10.

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Radioactive data
 
-This is EleutherAI's reimplementation of the paper ["Radioactive data: tracing through training"](https://arxiv.org/abs/2002.00937). Their GitHub repo can be found [here](https://github.com/facebookresearch/radioactive_data). **Warning:** the official open source implementation has some bugs, as well as miscellaneous issues like hard-coded paths that will prevent you from running it. To run the code, use the installation instructions below and then open `notebook.ipynb`
+This is EleutherAI's reimplementation of the paper ["Radioactive data: tracing through training"](https://arxiv.org/abs/2002.00937). 
 
-**Note:** @researcher2 is almost done with a full refactor so don't go getting too comfortable with the current code. 
+Their GitHub repo can be found [here](https://github.com/facebookresearch/radioactive_data). 
+**Warning:** The official open source implementation has a fairly complicated framework, as well as miscellaneous issues like hard-coded paths that will prevent you from running it. 
 
-# Original README
+We spent a fair amount of time getting the original implementation working with CIFAR10, then chose to do a full refactor after understanding the core requirements.
 
-At some point this will be rewritten, but for now we are preserving the original README.
+Our implementation is a refined version designed to demonstrate the marking, training and detection steps using the CIFAR10 dataset, with each stage self contained within its own python module. We have added logging to TensorBoard for your visualization enjoyment.
+
+Prior to implementing the training stage we created a full working example of a resnet18 classifer trained on CIFAR10. This can be used to benchmark your particular hardware and choose a good optimizer prior to running the main code. It's also a good starting point for ML beginners.
 
 ## Install
 
@@ -24,96 +27,13 @@ After that use pip to install other requirements:
 pip install -r requirements.txt
 ```
 
-## Creating radioactive data
+If anything is missing, simply pip install [missing requirement].
 
-Marking the data is very easy.
-First, specify a marking model
-```python
-import torch
-from torchvision import models
+## Running
 
-resnet18 = models.resnet18(pretrained=True)
-torch.save({
-    "model": resnet18.state_dict(),
-    "params": {
-      "architecture": "resnet18",
-      "num_classes" : ????
-    }
-  }, "pretrained_resnet18.pth")
+Please follow the basic_example.ipynb example. 
 
-```
-
-This seems like extra work for nothing. We do the sampling here across both dimensions and it ends up just getting sliced out inside make_data_radioactive.py with final dimension as (1, dim) as expected??
-
-Then sample random (normalized) directions as carriers:
-```python
-import torch
-
-n_classes, dim = 1000, 512
-carriers = torch.randn(n_classes, dim)
-carriers /= torch.norm(carriers, dim=1, keepdim=True)
-torch.save(carriers, "carriers.pth")
-```
-
-The `make_data_radioactive.py` script does the actual marking.
-For example, to mark images `img1.jpeg` and `img2.jpeg` with the carrier #0 (which should be the same as the class id), do:
-```
-python make_data_radioactive.py \
---carrier_id 0 \
---carrier_path carriers.pth \
---data_augmentation random \
---epochs 90 \
---img_paths img1.jpeg,img2.jpeg \
---lambda_ft_l2 0.01 \
---lambda_l2_img 0.0005 \
---marking_network pretrained_resnet18.pth \
---dump_path /path/to/images \
---optimizer sgd,lr=1.0
-```
-
-This takes about 1 minute and the output images are stored in /path/to/images.
-
-
-## Training a model
-
-The training is not controlled by the adversary. 
-However, to simulate it, we perform a standard training as follows.
-
-To train a model, we need to create a file that lists all the images that have been replaced with marked versions:
-
-```python
-import torch
-
-torch.save({
-  'type': 'per_sample',
-  'content': {
-    988: 'img1_radio.npy',
-  }
-}, "radioactive_data.pth")
-```
-
-We can then launch training:
-```
-python train-classif.py \
---architecture resnet18 \
---dataset imagenet \
---epochs 90 \
---train_path radioactive_data.pth \
---train_transform random
-```
-
-## Detecting if a model is radioactive
-
-So now we use the carriers and the trained network to detect the radioactive marks.
-
-```
-python detect_radioactivity.py \
---carrier_path carriers.pth \
---marking_network pretrained_resnet18.pth \
---tested_network checkpoint-0.pth
-```
-
-On the output, you should obtain a line with "log10(p)=...", which gives the (log of the) p-value of radioactivity detection.
+For any other questions please visit our Discord!
 
 ## License
 
--- detect_radioactivity.py
+++ detect_radioactivity.py
@@ -7,177 +7,157 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import argparse
-from src.model import build_model
-from src.stats import cosine_pvalue
 from scipy.stats import combine_pvalues
+from scipy.special import betainc
 import time
-import toml
+import os
+import torchvision.transforms.transforms as transforms
 import torchvision
 
-from src.dataset import NORMALIZE_CIFAR
-from src.net import extractFeatures
+from utils import NORMALIZE_CIFAR, Timer
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("--batch_size", default=256)
-#parser.add_argument("--carrier_path", type=str, required=True)
-#parser.add_argument("--crop_size", default=224)
-#parser.add_argument("--dataset", default="imagenet")
-#parser.add_argument("--img_size", default=256)
-#parser.add_argument("--marking_network", type=str, required=True)
-#parser.add_argument("--nb_workers", default=20)
-#parser.add_argument("--num_classes", default=1000)
-#parser.add_argument("--seed", default=1)
-#parser.add_argument("--target_network", type=str, required=True)
-#params = parser.parse_args()
+import logging
+from logger import setup_logger
+logger = logging.getLogger(__name__)
 
-class Params:
-    def __init__(self):
-        self.dataset = "cifar10"
-        self.dataset_root = ""
-        
-        self.crop_size = 224
-        self.img_size = 256
+def cosine_pvalue(c, d):
+    """
+    Given a dimension d, returns the probability that the dot product between
+    random unitary vectors is higher than c
+    """
+    assert type(c) in [float, np.float64, np.float32]
 
-        self.carrier_path = ""
-        self.marking_network = ""
-        self.target_network = ""
+    a = (d - 1) / 2.
+    b = 1 / 2.
 
-        self.batch_size = 64
-        self.nb_workers = 20
-        self.seed = 1
+    if c >= 0:
+        return 0.5 * betainc(a, b, 1-c**2)
+    else:
+        return 1 - cosine_pvalue(-c, d=d)
 
-def loadParams():
-    #with open("config_detect_radioactivity.toml", "w") as fh:
-    #    toml.dump(params.__dict__, fh)
-    #exit(0)
+# Direct copy from original codebase.
+# Less complicated then it looks. Just loops through all images in the dataset
+# running them through the model in batches. They are saved in an array of dimension
+# (num_images, final_layer_neurons). Offset stuff just handles copying each batch into the
+# right place. Targets are saved in a similar shaped array using the same process (unused here).
+def extract_features(loader, model, device, ignore_first=False, numpy=False, verbose=True):
+    assert not model.training
+    with torch.no_grad():
+        n = len(loader.dataset)
+        features = None
+        # targets = np.zeros((n), dtype=int)
 
-    params = Params()
-    with open("config_detect_radioactivity.toml", "r") as fh:
-        loaded = toml.load(fh)
-        for k, v in loaded.items():
-            params.__dict__[k] = v
+        offset = 0
+        start = time.time()
+        for elements in loader:
+            if ignore_first:
+                elements = elements[1:]
 
-    assert(params.carrier_path != "")
-    assert(params.marking_network != "")
-    assert(params.target_network != "")
+            img = elements[0]
+            ft = model(img.to(device, non_blocking=True))
+            sz = img.size(0)
 
-    return params
+            if features is None:
+                d = ft.size(1)
+                # features = np.zeros((n, d), dtype=np.float32)
+                features = torch.zeros((n, d), dtype=torch.float32)
+                all_targets = [torch.zeros((n, ), dtype=int) for _ in elements[1:]]
 
-if __name__ == "__main__":
+            features[offset:offset+sz] = ft.cpu().detach()#.numpy()
 
-    params = loadParams()
+            for target, targets in zip(elements[1:], all_targets):
+                targets[offset:offset+sz] = target#.numpy()
 
+            offset += sz
+            if offset % (100 * sz) == 0 and verbose:
+                speed = offset / (time.time() - start)
+                eta = (len(loader)*sz - offset) / speed
+                print(f"Speed: {speed}, ETA: {eta}")
+
+            # if offset >= 20000 and n >= 1e6:
+            #     for i in range(10):
+            #         print(20 * "*")
+            #     print("WARNING: Quit extractFeatures before having extracted features of all samples")
+            #     for i in range(10):
+            #         print(20 * "*")
+            #     break
+
+        assert offset == n
+        # if offset < n:
+        #     features = features[:offset]
+        #     all_targets = tuple([targets[:offset] for targets in all_targets])
+
+        if numpy:
+            features = features.numpy()
+            all_targets = [targets.numpy() for targets in all_targets]
+
+        return (features,) + tuple(all_targets)
+
+def get_data_loader(batch_size, num_workers):
+
+    dataset_directory = "experiments/datasets"
+    test_transform = transforms.Compose([transforms.ToTensor(), NORMALIZE_CIFAR])
+    test_set = torchvision.datasets.CIFAR10(dataset_directory, train=False, transform=test_transform)
+    test_set_loader = torch.utils.data.DataLoader(test_set, 
+                                                  batch_size=batch_size, 
+                                                  num_workers=num_workers, 
+                                                  shuffle=False,
+                                                  pin_memory=True)
+
+    return test_set_loader
+
+def main(batch_size=256, num_workers=1):
+
+    # Setup paths and logger
+    experiment_directory = "experiments/radioactive/"
+    output_directory = os.path.join(experiment_directory, "detect_radioactivity")
+    logfile_path = os.path.join(output_directory, "logfile.txt")
+    carrier_path = os.path.join(experiment_directory, "carriers.pth")
+    marked_network_path = "experiments/radioactive/train_marked_classifier/checkpoint.pth"
+    
+    os.makedirs(output_directory, exist_ok=True)
+    setup_logger(logfile_path)
+
+    # Setup Device
     use_cuda = torch.cuda.is_available()
     print(f"CUDA Available? {use_cuda}")
     device = torch.device("cuda" if use_cuda else "cpu")
 
     # Setup Dataloader
-    valid_data_loader = None
-    if params.dataset.lower() == "cifar10":
-        transforms = torchvision.transforms.transforms.Compose([
-            torchvision.transforms.transforms.Resize(params.img_size),
-            torchvision.transforms.transforms.CenterCrop(params.crop_size),
-            torchvision.transforms.transforms.ToTensor(),
-            NORMALIZE_CIFAR,
-            ])
-
-        valid_dataset = torchvision.datasets.CIFAR10(root=params.dataset_root, train=False, download=True, transform=transforms)
-
-        valid_data_loader = torch.utils.data.DataLoader(
-            valid_dataset,
-            batch_size=params.batch_size,
-            shuffle=False,
-            num_workers=params.nb_workers,
-            pin_memory=True,
-            sampler=None
-        )
-    else:
-        raise ValueError(f"{params.dataset} dataloader not implemented, please change dataset config.")
+    test_set_loader = get_data_loader(batch_size, num_workers)
 
     # Load Carrier
-    carrier = torch.load(params.carrier_path).numpy()
+    carrier = torch.load(carrier_path).numpy()
 
-    # Load marking network & remove fully connected layer
-    print(f"Loading marking network '{params.marking_network}'")
-    marking_ckpt = torch.load(params.marking_network)
-    marking_arch = marking_ckpt['params']['architecture']
-    marking_classes = marking_ckpt['params']['num_classes']
-    print(f"Marking network architecture: {marking_arch}")
-    print(f"Marking network original classes: {marking_classes}")   
-    marking_network = build_model(marking_arch, marking_classes).eval().to(device)
-
-    print("Removing marking network fully connected layer - save weights for later first")
+    # Recreate marking network and remove fully connected layer
+    marking_network = torchvision.models.resnet18(pretrained=True)
     marking_network.fc = nn.Sequential()
-    marking_state = marking_ckpt['model']
-    W_old = marking_state['fc.weight'].cpu().numpy()
-    del marking_state['fc.weight']
-    del marking_state['fc.bias']
+    marking_network.to(device)
+    marking_network.eval()    
 
-    print(marking_network.load_state_dict(marking_state, strict=False))
-    
-    # Start Timer
-    start_all = time.time()
+    t = Timer()
+    t.start()
 
     # Load Target Network and remove fully connected layer
-    # when running train-classif.py, trainer.py dumps the entire global params into the model save
-    print(f"Loading target network '{params.target_network}'")
-    target_ckpt = torch.load(params.target_network)
-    target_arch = target_ckpt['params']['architecture'] 
-    target_classes = target_ckpt['params']['num_classes']
-    print(f"Target network architecture: {target_arch}")
-    print(f"Target network classes: {target_classes}")
-    target_network = build_model(target_arch, target_classes).eval().to(device)
-
-    print("Removing target network fully connected layer")
-    tested_state = {k.replace("module.", ""): v for k, v in target_ckpt['model'].items()}
-
-    if target_arch.startswith("resnet"):
-        target_network.fc = nn.Sequential()
-        del tested_state['fc.weight']
-        del tested_state['fc.bias']
-    elif target_arch.startswith("vgg"):
-        target_network.classifier[6] = nn.Sequential()
-        del tested_state['classifier.6.weight']
-        del tested_state['classifier.6.bias']
-    elif target_arch.startswith("densenet"):
-        target_network.classifier = nn.Sequential()
-        del tested_state['classifier.weight']
-        del tested_state['classifier.bias']
-
-    print(target_network.load_state_dict(tested_state, strict=False))
+    target_checkpoint = torch.load(marked_network_path)
+    target_network = torchvision.models.resnet18(pretrained=False, num_classes=10)
+    target_network.load_state_dict(target_checkpoint["model_state_dict"])
+    target_network.fc = nn.Sequential()
+    target_network.to(device)
+    target_network.eval()
 
     # Extract features
-    start = time.time()
-    print("Commence feature extraction on marking network...", end="")
-    features_marker, _ = extractFeatures(valid_data_loader, marking_network, device, verbose=False)
-    print("done")
-    print("Commence feature extraction on target network...", end="")
-    features_tested, _  = extractFeatures(valid_data_loader, target_network, device, verbose=False)
-    print("done")
-    print("Extracting features took %.2f" % (time.time() - start))
-    features_marker = features_marker.numpy()
-    features_tested = features_tested.numpy()
+    logger.info("Extracting image features after running through marking and target networks.")
+    features_marking, _ = extract_features(test_set_loader, marking_network, device, verbose=False)
+    features_target, _  = extract_features(test_set_loader, target_network, device, verbose=False)
+    features_marking = features_marking.numpy()
+    features_target = features_target.numpy()
 
     # Align spaces
-    X, residuals, rank, s = np.linalg.lstsq(features_marker, features_tested)
-    print("Norm of residual: %.4e" % np.linalg.norm(np.dot(features_marker, X) - features_tested)**2)
+    X, residuals, rank, s = np.linalg.lstsq(features_marking, features_target)
+    print("Norm of residual: %.4e" % np.linalg.norm(np.dot(features_marking, X) - features_target)**2)
 
-    # Put classification vectors into aligned space
-    if target_arch.startswith("resnet"):
-        key = 'fc.weight'
-        if key not in target_ckpt['model']:
-            key = 'module.fc.weight'
-    elif target_arch == "vgg16":
-        key = 'classifier.6.weight'
-        if key not in target_ckpt['model']:
-            key = 'module.classifier.6.weight'
-    elif target_arch.startswith("densenet"):
-        key = 'classifier.weight'
-        if key not in target_ckpt['model']:
-            key = 'module.classifier.weight'
-
-    W = target_ckpt['model'][key].cpu().numpy()
+    W = target_checkpoint["model_state_dict"]["fc.weight"].cpu().numpy()
     W = np.dot(W, X.T)
     W /= np.linalg.norm(W, axis=1, keepdims=True)
 
@@ -185,9 +165,13 @@ if __name__ == "__main__":
     scores = np.sum(W * carrier, axis=1)
 
     print("Mean p-value is at %d times sigma" % int(scores.mean() * np.sqrt(W.shape[0] * carrier.shape[1])))
-    print("Epoch of the model: %d" % target_ckpt["epoch"])
+    print("Epoch of the model: %d" % target_checkpoint["epoch"])
 
     p_vals = [cosine_pvalue(c, d=carrier.shape[1]) for c in list(scores)]
     print(f"log10(p)={np.log10(combine_pvalues(p_vals)[1])}")
 
-    print("Total took %.2f" % (time.time() - start_all))
+    elapsed_time = t.stop()
+    print("Total took %.2f" % (elapsed_time))
+
+if __name__ == '__main__':
+    main()
